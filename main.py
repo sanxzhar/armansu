22eb134460c3da84b78614e1126b12c736400d94
Sanzhar <sanxzhar@gmail.com>
2023-07-13 14:20:30 +0600

    init

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 import os
-import streamlit as st
+from fastapi import FastAPI
+from pydantic import BaseModel
 from dotenv import load_dotenv
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
+from fastapi.middleware.cors import CORSMiddleware
 
-def main():
+
+def openai(text: str):
     load_dotenv()
-    st.set_page_config(page_title="@armansu")
-    st.header("Ask your question from @Armansu")
+
     os.getenv("OPENAI_API_KEY")
-    
+
     embeddings = OpenAIEmbeddings()
 
     knowledge_base = FAISS.load_local("./storage/index3", embeddings)
-    user_question = st.text_input("Type your question")
-    if user_question:
-        chain = RetrievalQA.from_chain_type(llm = ChatOpenAI(model_name="gpt-3.5-turbo"), chain_type="stuff", retriever = knowledge_base.as_retriever())
-        response = chain.run(user_question)
-        st.write(response)
+    chain = RetrievalQA.from_chain_type(llm = ChatOpenAI(model_name="gpt-3.5-turbo"), chain_type="stuff", retriever = knowledge_base.as_retriever())
+    response = chain.run(text)
+    return(response)
 
-if __name__ == "__main__":
-    main()
+class QueryModel(BaseModel):
+    text: str
 
+app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post('/query')
+async def response(query: QueryModel):
+    text = query.text
+    response = openai(text)
+    return{"status": 200, "response" : response}
 
 
